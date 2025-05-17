1f2fd67777d660a0ec928cd1b9b99c3fd5a98afd
Yaroslav Zhuravlov <yaroslav.zhuravlov.dev@gmail.com>
2025-05-17 16:14:59 +0300

    add rest_framework to INSTALLED_APPS

--- config/settings.py
+++ config/settings.py
@@ -39,6 +39,9 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    # third-party
+    "rest_framework",
+    # local
 ]
 
 MIDDLEWARE = [
