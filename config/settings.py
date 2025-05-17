4147c57caaa6036c1bfdff83937ee576686d4b51
Yaroslav Zhuravlov <yaroslav.zhuravlov.dev@gmail.com>
2025-05-17 18:45:41 +0300

    add session authentication in DUBUG mode

--- config/settings.py
+++ config/settings.py
@@ -131,3 +131,12 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+REST_FRAMEWORK = {
+    "DEFAULT_AUTHENTICATION_CLASSES": [],
+}
+
+if DEBUG:
+    REST_FRAMEWORK["DEFAULT_AUTHENTICATION_CLASSES"].append(
+        "rest_framework.authentication.SessionAuthentication"
+    )
