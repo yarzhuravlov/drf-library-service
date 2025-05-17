200a47b949401d0fe6f31c09fa1fd98d30b49cd5
Yaroslav Zhuravlov <yaroslav.zhuravlov.dev@gmail.com>
2025-05-17 19:35:41 +0300

    add borrowings app with Borrowing model

---
+++ borrowings/models.py
@@ -0,0 +1,17 @@
+from django.contrib.auth import get_user_model
+from django.db import models
+
+from books.models import Book
+
+User = get_user_model()
+
+
+class Borrowing(models.Model):
+    borrow_date = models.DateField()
+    expected_return = models.DateField()
+    actual_return = models.DateField()
+    book = models.ForeignKey(Book, on_delete=models.PROTECT)
+    user = models.ForeignKey(User, on_delete=models.PROTECT)
+
+    class Meta:
+        default_related_name = "borrowings"
