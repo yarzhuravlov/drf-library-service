7376504b04eed968d32fb360e084e465b076fd11
Liubomyr Levkovych <liubomyrlevkovich@gmail.com>
2025-05-19 11:59:55 +0300

    add book & author views with CRUD

---
+++ books/views.py
@@ -0,0 +1,13 @@
+from rest_framework import viewsets
+from .models import Book, Author
+from .serializers import BookSerializer, AuthorSerializer
+
+
+class AuthorViewSet(viewsets.ModelViewSet):
+    queryset = Author.objects.all()
+    serializer_class = AuthorSerializer
+
+
+class BookViewSet(viewsets.ModelViewSet):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
