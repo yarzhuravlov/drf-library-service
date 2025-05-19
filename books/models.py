cf01c668fd3789cd2de54ea07c1bb475de31e010
Liubomyr Levkovych <liubomyrlevkovich@gmail.com>
2025-05-19 15:14:36 +0300

    changed relation many to many in books & authors, redefined str method in Book & Author models

--- books/models.py
+++ books/models.py
@@ -5,6 +5,9 @@ class Author(models.Model):
     first_name = models.CharField(max_length=63)
     last_name = models.CharField(max_length=63)
 
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
+
 
 class Book(models.Model):
     class Covers(models.TextChoices):
@@ -12,11 +15,13 @@ class Book(models.Model):
         SOFT = "soft"
 
     title = models.CharField(max_length=255)
-    author = models.ForeignKey(
+    authors = models.ManyToManyField(
         Author,
         related_name="books",
-        on_delete=models.PROTECT,
     )
     cover = models.CharField(max_length=4, choices=Covers.choices)
     inventory = models.PositiveIntegerField()
     daily_fee = models.PositiveIntegerField()
+
+    def __str__(self):
+        return self.title
