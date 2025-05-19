a588ad59fa00204f248cd34a3c8c26fb965f55af
Nikita Tkachenko <7heswatgg@gmail.com>
2025-05-19 12:36:24 +0200

    add permission classes to viewset

--- borrowings/views.py
+++ borrowings/views.py
@@ -1,4 +1,5 @@
 from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
 
 from borrowings.models import Borrowing
 from borrowings.serializers import (
@@ -9,6 +10,7 @@ from borrowings.serializers import (
 
 class BorrowingViewSet(viewsets.ModelViewSet):
     queryset = Borrowing.objects.all()
+    permission_classes = (IsAuthenticated,)
 
     def get_serializer_class(self):
         if self.action == 'list':
