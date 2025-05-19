2b3c8706717db8f98dbb3a5d6b72bc477d87f6fa
Liubomyr Levkovych <liubomyrlevkovich@gmail.com>
2025-05-19 11:58:02 +0300

    add book & author serializers

---
+++ books/serializers.py
@@ -0,0 +1,29 @@
+from rest_framework import serializers
+from .models import Author, Book
+
+
+class AuthorSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Author
+        fields = ("id", "first_name", "last_name")
+
+
+class BookSerializer(serializers.ModelSerializer):
+    author = AuthorSerializer(read_only=True)
+    author_id = serializers.PrimaryKeyRelatedField(
+        queryset=Author.objects.all(),
+        source="author",
+        write_only=True,
+    )
+
+    class Meta:
+        model = Book
+        fields = (
+            "id",
+            "title",
+            "author",
+            "author_id",
+            "cover",
+            "inventory",
+            "daily_fee"
+        )
