608392ec1d253297f7a721ee6844e837da8e6766
Nikita Tkachenko <7heswatgg@gmail.com>
2025-05-19 14:10:45 +0200

    add is_active filter and optional user_id filter to endpoint params

--- borrowings/views.py
+++ borrowings/views.py
@@ -10,13 +10,29 @@ from borrowings.serializers import (
 
 
 class BorrowingViewSet(viewsets.ModelViewSet):
+    queryset = Borrowing.objects.all()
     permission_classes = (IsAuthenticated,)
 
     def get_queryset(self):
         user = self.request.user
         if user.is_staff:
-            return Borrowing.objects.all()
-        return Borrowing.objects.filter(user=user)
+            queryset = self.queryset
+        else:
+            queryset = self.queryset.filter(user=user)
+
+        is_active_param = self.request.query_params.get("is_active")
+        user_id_param = self.request.query_params.get("user_id")
+
+        if user.is_staff and user_id_param:
+            queryset = self.queryset.filter(user__id=user_id_param)
+
+        if is_active_param is not None:
+            if is_active_param.lower() == "true":
+                queryset = queryset.filter(actual_return__isnull=True)
+            elif is_active_param.lower() == "false":
+                queryset = queryset.filter(actual_return__isnull=False)
+
+        return queryset
 
     def get_serializer_class(self):
         if self.action == "list":
