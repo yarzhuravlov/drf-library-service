e563bbde2e85853bcb0c2f34fc228cc78637e286
Liubomyr Levkovych <liubomyrlevkovich@gmail.com>
2025-05-19 13:40:49 +0300

    add books app to config/urls & add book & author to admin

--- config/urls.py
+++ config/urls.py
@@ -23,6 +23,7 @@ from django.urls import path, include
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/v1/", include("accounts.urls")),
+    path("api/v1/", include("books.urls")),
     path("api/v1/auth/", include("dj_rest_auth.urls")),  # login/logout/password reset
     path("api/v1/auth/registration/", include("dj_rest_auth.registration.urls")),  # registration
 ]
