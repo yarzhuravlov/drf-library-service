86b2c8e8e7afc45d7055ee8fa033b462c4fc3ff3
Dmytro Stehantsev <stegan296@gmail.com>
2025-05-21 10:04:06 +0300

    fixed tests + urls

--- config/urls.py
+++ config/urls.py
@@ -32,7 +32,6 @@ urlpatterns = [
     path("api/v1/auth/", include("djoser.urls")),
     path("api/v1/auth/", include("djoser.urls.jwt")),
     path("api/v1/auth/", include("djoser.social.urls")),
-    # path('api/v1/auth/', include('accounts.urls')),
 
     path("api/v1/books/", include("books.urls")),
     path("api/v1/", include("borrowings.urls")),
