226a900b6e00da9f099db246d158721e830d8d18
Liubomyr Levkovych <liubomyrlevkovich@gmail.com>
2025-05-21 11:31:53 +0300

    add validation book has at least one author & test, created open api docs

--- books/views.py
+++ books/views.py
@@ -1,15 +1,18 @@
+from drf_spectacular.utils import extend_schema
 from rest_framework import viewsets
 from books.models import Book, Author
 from books.permissions import IsAdminOrReadOnly
 from books.serializers import BookSerializer, AuthorSerializer
 
 
+@extend_schema(tags=["Authors"])
 class AuthorViewSet(viewsets.ModelViewSet):
     queryset = Author.objects.all()
     serializer_class = AuthorSerializer
     permission_classes = [IsAdminOrReadOnly]
 
 
+@extend_schema(tags=["Books"])
 class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
