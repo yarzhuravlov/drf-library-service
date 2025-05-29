1f1fbdc58cb31874a6b569445c9ab2812c31d18e
Liubomyr Levkovych <liubomyrlevkovich@gmail.com>
2025-05-29 11:47:42 +0300

    fixed potential N + 1 in books

--- books/admin.py
+++ books/admin.py
@@ -6,6 +6,10 @@ from books.models import Author, Book
 class BookAdmin(admin.ModelAdmin):
     list_display = ("title", "get_authors", "cover", "inventory", "daily_fee")
 
+    def get_queryset(self, request):
+        queryset = super().get_queryset(request)
+        return queryset.prefetch_related("authors")
+
     def get_authors(self, obj):
         return ", ".join(str(author) for author in obj.authors.all())
     get_authors.short_description = "Authors"
