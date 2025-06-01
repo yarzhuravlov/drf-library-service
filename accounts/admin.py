a8b7532bb6c124da9646bac2fece4c373ca2ecaa
Dmytro Stehantsev <stegan296@gmail.com>
2025-06-01 19:10:05 +0300

    flack8

--- accounts/admin.py
+++ accounts/admin.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from accounts.models import User
 
+
 @admin.register(User)
 class UserAdmin(BaseUserAdmin):
 
